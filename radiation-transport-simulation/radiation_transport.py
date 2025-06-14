263aac1d65d0a38c4b40d4f63d8eedab71ee58c2
GitMaster9 <karlosecen10@gmail.com>
2025-06-14 14:23:06 +0200

    Added save and load file to radiation tranport simulations

--- radiation-transport-simulation/radiation_transport.py
+++ radiation-transport-simulation/radiation_transport.py
@@ -1,6 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import time
+import h5py
 
 class Simulation:
     def __init__(self, num_photons = 100_000, slab_thickness = 10.0, attenuation_coeff = 0.1, use_scatter = False, p_scatter = 0.7, p_scatter_reverses_direction = 0.5, seed = 1):
@@ -24,7 +24,7 @@ class Simulation:
         self.absorbed_fraction = 0.0
         
         self.escaped_count = 0
-        self.escaped_fraction = 0
+        self.escaped_fraction = 0.0
         
         self.time_needed = 0.0
 
@@ -83,3 +83,64 @@ class Simulation:
         
         self.absorbed_fraction = self.absorbed_count / self.num_photons
         self.escaped_fraction = escaped_count / self.num_photons
+
+    def save_to_file(self, file_name: str):
+        file_path = f"{file_name}.h5"
+        with h5py.File(file_path, "w") as f:
+            f.attrs["num_photons"] = self.num_photons
+            f.attrs["slab_thickness"] = self.slab_thickness
+            f.attrs["attenuation_coeff"] = self.attenuation_coeff
+            f.attrs["use_scatter"] = self.use_scatter
+            f.attrs["p_scatter"] = self.p_scatter
+            f.attrs["p_scatter_reverses_direction"] = self.p_scatter_reverses_direction
+            f.attrs["seed"] = self.seed
+
+            f.create_dataset("absorbed_positions", data=self.absorbed_positions, compression="gzip")
+            f.attrs["absorbed_count"] = self.absorbed_count
+            f.attrs["absorbed_fraction"] = self.absorbed_fraction
+            
+            f.attrs["escaped_count"] = self.escaped_count
+            f.attrs["escaped_fraction"] = self.escaped_fraction
+
+            f.attrs["time_needed"] = self.time_needed
+
+    def load_from_file(self, file_name: str):
+        try:
+            with h5py.File(f"{file_name}.h5", "r") as f:
+                num_photons = int(f.attrs["num_photons"])
+                slab_thickness = float(f.attrs["slab_thickness"])
+                attenuation_coeff = float(f.attrs["attenuation_coeff"])
+                use_scatter = bool(f.attrs["use_scatter"])
+                p_scatter = float(f.attrs["p_scatter"])
+                p_scatter_reverses_direction = float(f.attrs["p_scatter_reverses_direction"])
+                seed = int(f.attrs["seed"])
+
+                absorbed_positions = list(f["absorbed_positions"][:])
+                absorbed_count = int(f.attrs["absorbed_count"])
+                absorbed_fraction = float(f.attrs["absorbed_fraction"])
+
+                escaped_count = int(f.attrs["escaped_count"])
+                escaped_fraction = float(f.attrs["escaped_fraction"])
+
+                time_needed = float(f.attrs["time_needed"])
+                
+        except Exception as e:
+            print("Can't load simulation from file: ", e)
+            return
+        
+        self.num_photons = num_photons
+        self.slab_thickness = slab_thickness
+        self.attenuation_coeff = attenuation_coeff
+        self.use_scatter = use_scatter
+        self.p_scatter = p_scatter
+        self.p_scatter_reverses_direction = p_scatter_reverses_direction
+        self.seed = seed
+        
+        self.absorbed_positions = absorbed_positions
+        self.absorbed_count = absorbed_count
+        self.absorbed_fraction = absorbed_fraction
+        
+        self.escaped_count = escaped_count
+        self.escaped_fraction = escaped_fraction
+
+        self.time_needed = time_needed
